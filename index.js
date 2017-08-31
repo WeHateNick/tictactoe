8a176fc4d6b1fc64ef07385191301ca7153e8282
Nick Ocampo <nick@wehatenick.com>
2017-08-31 16:38:35 -0700

    set up the first turn prompt

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 'use strict';
 
-var chalk       = require('chalk');
-var clear       = require('clear');
-var CLI         = require('clui');
-var figlet      = require('figlet');
-var inquirer    = require('inquirer');
-var fs          = require('fs');
+const chalk       = require('chalk');
+const clear       = require('clear');
+const CLI         = require('clui');
+const figlet      = require('figlet');
+const inquirer    = require('inquirer');
+const fs          = require('fs');
 
+var board = `
+	      A    B    C
+
+	    +----+----+----+
+	1   | A1 | B1 | C1 |
+	    +----+----+----+
+	2   | A2 | B2 | C2 |
+	    +----+----+----+
+	3   | A3 | B3 | C3 |
+	    +----+----+----+
+`
+var availableCells = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3'];
+
+let turnOptions = [
+	{
+	  type: 'list',
+	  name: 'cell',
+	  message: 'Select a cell where you would like to place your mark',
+	  choices: availableCells
+	}
+];
 
 let playerOptions = [
 	{
@@ -16,6 +37,8 @@ let playerOptions = [
 	  choices: ['Player X', 'Player O']
 	}
 ];
+
+
 
 function init () {
 	clear();
@@ -28,9 +51,23 @@ function init () {
 	  chalk.yellow('Welcome to Nick\'s Tic Tac Toe')
 	);
 	inquirer.prompt(playerOptions).then(function (answer) {
+		clear();
 		console.log(chalk.yellow('Great. You selected', answer.player));
+		startGame();
 	})
 }
+function showTurnOptions () {
+	inquirer.prompt(turnOptions).then(function (answer) {
+
+	})
+}
+
+function startGame () {
+	console.log(chalk.blue('You\'re up first. Select a place to put your mark on'));
+	console.log(chalk.grey(board));
+	showTurnOptions();
+}
+
 init();
 
 
