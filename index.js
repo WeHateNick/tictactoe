a5e26644732e89725d1af970a342ec4287fa5ea9
Nick Ocampo <nick@wehatenick.com>
2017-08-31 17:27:48 -0700

    set up cell selection

--- index.js
+++ index.js
@@ -7,18 +7,21 @@ const figlet      = require('figlet');
 const inquirer    = require('inquirer');
 const fs          = require('fs');
 
-var board = `
-	      A    B    C
+var player1Symbol, player2Symbol;
 
-	    +----+----+----+
-	1   | A1 | B1 | C1 |
-	    +----+----+----+
-	2   | A2 | B2 | C2 |
-	    +----+----+----+
-	3   | A3 | B3 | C3 |
-	    +----+----+----+
-`
 var availableCells = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3'];
+
+var boardValues = {
+	a1: 'A1',
+	a2: 'A2',
+	a3: 'A3',
+	b1: 'B1',
+	b2: 'B2',
+	b3: 'B3',
+	c1: 'C1',
+	c2: 'C2',
+	c3: 'C3',
+};
 
 let turnOptions = [
 	{
@@ -38,8 +41,6 @@ let playerOptions = [
 	}
 ];
 
-
-
 function init () {
 	clear();
 	console.log(
@@ -53,19 +54,37 @@ function init () {
 	inquirer.prompt(playerOptions).then(function (answer) {
 		clear();
 		console.log(chalk.yellow('Great. You selected', answer.player));
+		player1Symbol = answer.player === 'Player X' ? 'X' : 'O';
 		startGame();
 	})
 }
+function showBoard () {
+	console.log(chalk.yellow(`
+		      A    B    C
+
+		    +----+----+----+
+		1   | ${boardValues.a1} | ${boardValues.b1} | ${boardValues.c1} |
+		    +----+----+----+
+		2   | ${boardValues.a2} | ${boardValues.b2} | ${boardValues.c2} |
+		    +----+----+----+
+		3   | ${boardValues.a3} | ${boardValues.b3} | ${boardValues.c3} |
+		    +----+----+----+
+	`));
+}
+function startGame () {
+	console.log(chalk.blue(`You\'re up first. Select a place to put your "${player1Symbol}" mark on`));
+	showBoard();
+	showTurnOptions();
+}
 function showTurnOptions () {
 	inquirer.prompt(turnOptions).then(function (answer) {
-
+		playerTurn(answer.cell.toLowerCase());
 	})
 }
-
-function startGame () {
-	console.log(chalk.blue('You\'re up first. Select a place to put your mark on'));
-	console.log(chalk.grey(board));
-	showTurnOptions();
+function playerTurn (cell) {
+	console.log(`You selected ${cell}`);
+	boardValues[cell] = `${player1Symbol} `;
+	showBoard();
 }
 
 init();
